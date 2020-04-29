742492bbeb4138bfbe480ef7740819b79207cd11
Nicholas Taylor <nicholas.taylor1410@gmail.com>
2020-04-29 23:23:47 +1000

    feat: adding Dockerfile and build

--- jenkins/jobs/JobDsl.groovy
+++ jenkins/jobs/JobDsl.groovy
@@ -1,6 +1,28 @@
 
-job("Bake_Jenkins_Container") {
+job('Bake_Jenkins_Container') {
+    
+    wrappers {
+        environmentVariables {
+            env('SCOPE', 'nick')
+        }
+        credentialsBinding {
+            string('GH_PAT', 'GH_PAT')
+            string('GH_CLIENT_ID', 'GH_CLIENT_ID')
+            string('GH_CLIENT_SECRET', 'GH_CLIENT_SECRET')
+        }
+    }
+
+    scm {
+        git {
+            remote {
+                github('ProgrammerNick/jenkins', 'https')
+                credentials('GH_PAT')
+            }
+            branch('master')
+        }
+    }
+    
     steps {
-        shell("echo Hello, World!")
+        shell("docker build . -t ${SCOPE}/jenkins")
     }
 }
